1b1952a9c1d1949f0ca6b53a7a40132f6e5df5d9
MaksymKalennyk <107300165+MaksymKalennyk@users.noreply.github.com>
2023-10-09 13:18:29 +0300

    first commit

---
+++ src/main/java/com/example/joybot/JoyBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.joybot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoyBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JoyBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/joybot/JoyBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.joybot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoyBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
