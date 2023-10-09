8a47a37bd847c0bbf4254acd036db293908ecc14
MaksymKalennyk <107300165+MaksymKalennyk@users.noreply.github.com>
2023-10-09 13:29:42 +0300

    bot init

---
+++ src/main/java/com/example/joybot/bot/TelegramBot.java
@@ -0,0 +1,59 @@
+package com.example.joybot.bot;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+@AllArgsConstructor
+@NoArgsConstructor
+public class TelegramBot extends TelegramLongPollingBot {
+
+    @Value("${bot.name}")
+    String botName;
+
+    @Value("${bot.token}")
+    String token;
+
+    @Override
+    public String getBotUsername() {
+        return botName;
+    }
+
+    @Override
+    public String getBotToken() {
+        return token;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+            if ("/start".equals(messageText)) {
+                startCommandReceived(chatId);
+            }
+        }
+    }
+
+    private void startCommandReceived(Long chatId) {
+        String answer = "You look lonely, I can fix that";
+        sendMessage(chatId, answer);
+    }
+
+    private void sendMessage(Long chatId, String textToSend){
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(String.valueOf(chatId));
+        sendMessage.setText(textToSend);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
